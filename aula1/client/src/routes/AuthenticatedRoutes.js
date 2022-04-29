f4e69f09ed802ab83141dcaf7cb4c44de6ca9882
Vinicius Pegorini <viniciuspegorini@gmail.com>
2022-04-28 21:54:53 -0300

    aula 1 - client react router - 2022-04-28

--- aula1/client/src/routes/AuthenticatedRoutes.js
+++ aula1/client/src/routes/AuthenticatedRoutes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 import HomePage from '../pages/HomePage';
 
 const AuthenticatedRoutes = () => {
 
     return (
         <div>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 
